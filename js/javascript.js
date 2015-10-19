1d795364ca370d914c5ba8b28e3c7dbdc8b312d9
Johnny Chan <jc2johnny@gmail.com>
2015-10-19 15:06:47 -0400

    update

--- js/javascript.js
+++ js/javascript.js
@@ -164,6 +164,8 @@ var placeCardDealer = function(player){
 }
 };
 
+
+//http://jsfromhell.com/array/shuffle
 function shuffle(o){
       for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
